b0522983e32c3e918d899106072b2d7b0de81cf5
SAAD <s.meniar@edu.umi.ac.ma>
2024-05-12 21:34:18 +0100

    Correct Prompt_generator

---
+++ Analyser_Components/prompt_generator.py
@@ -0,0 +1,32 @@
+import google.generativeai as gai
+from PIL import Image
+import supervision as sv
+from tqdm.notebook import tqdm
+import pandas as pd
+
+
+class prompt_generator():
+        def __init__(self,model,key,images_dir = "./data/",images_extensions = ['jpg', 'jpeg', 'png']):
+            # self.model = model
+            self.model = gai.GenerativeModel(model)
+            self.key = key
+            self.images_dir = images_dir
+            self.images_extensions = images_extensions
+
+            gai.configure(api_key=self.key)
+            self.image_paths = sv.list_files_with_extensions(
+            directory=self.images_dir,
+            extensions=self.images_extensions)
+            
+            self.prompts_dict = {}
+
+        def generate_prompts(self,number_of_prompts=20):
+            for image_path in tqdm(self.image_paths):
+                image_name = image_path.stem  # 'stem' gives the file name without extension
+                image = Image.open(image_path)
+                self.prompts_dict[image_name] = []
+                for i in range(number_of_prompts):
+                    res = self.model.generate_content(image)  
+                    self.prompts_dict[image_name].append(res.text)
+
+            return self.prompts_dict
